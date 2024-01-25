0a9a2276b7c97cc75056b3b02719a2c60f85a600
ducmonmz <ducmonmz@gmail.com>
2024-01-25 09:59:39 +0700

    Add missing config files

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,11 @@
 import * as React from "react"
+import Header from "./header"
+import Footer from "./footer"
 
-const Layout = ({ children }: { children: React.ReactNode }) => <div className="global-wrapper">{children}</div>
+const Layout = ({ children }: { children: React.ReactNode }) => <div className="global-wrapper">
+    <Header></Header>
+    <main>{children}</main>
+    <Footer></Footer>
+</div>
 
 export default Layout
