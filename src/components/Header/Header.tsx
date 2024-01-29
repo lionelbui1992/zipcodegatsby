d0567a1f2477cdcdbb47c73b39c6ce0c1df1428a
hains1088 <hains@onextdigital.com>
2024-01-29 16:11:45 +0700

    update header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -30,15 +30,57 @@ export const Header = ({
 
   useEffect(() => {
     colorHeader(isHeaderBlack, mainLogoWhite, setMainLogo, setTextColorHeader);
-  }, []);
+
+    const sectionsBlack = document.querySelectorAll(".bg-black");
+    const sectionHeader = document.querySelector(".header");
+    // const isGsap = document.querySelector("#___gatsby") as HTMLElement;;
+
+    // if (isGsap) {
+    //   const transformStyle = isGsap.style.transform;
+    //   if (transformStyle && transformStyle.indexOf("matrix3d") !== -1) {
+    //     fix_scroll();
+    //   }
+    // }
+
+    const handleScroll = (sectionsBlack:any, sectionHeader:any) => {
+      if(sectionsBlack.length > 0) {
+          for (const sectionBlack of sectionsBlack) {
+          const rect = sectionBlack.getBoundingClientRect();
+
+          if (rect.top <= sectionHeader.offsetHeight/2 && rect.top + rect.height > sectionHeader.offsetHeight/2) {
+            setIsHeaderBlack(false);
+            setMainLogo(mainLogoWhite);
+            setTextColorHeader("#fff");
+            break;
+          } else {
+            setIsHeaderBlack(true);
+            setMainLogo(mainLogoBlack);
+            setTextColorHeader("#1E1E1E");
+          }
+        }
+    
+      }
+    }
+    handleScroll(sectionsBlack, sectionHeader);
+    window.addEventListener("scroll", () => handleScroll(sectionsBlack, sectionHeader));
+    
+  }, [mainLogoBlack, mainLogoWhite]);
 
   const handleMenuMobileClick = () => {
     setIsClickMenu(prevIsClickMenu => !prevIsClickMenu);
   }
 
+  // const fix_scroll = () => {
+  //   const s = window.scrollY;
+  //   console.log(s);
+  //   const fixedHeader = document.querySelector('.header');
+  //   fixedHeader.style.position = 'absolute';
+  //   fixedHeader.style.top = `${s}px`;
+  // };
+
   return (
     <header
-      className="header container"
+      className={`header container ${isClickMenu ? 'header__hidden' : 'header__visible'}`}
     >
       <Link
         to="/"
@@ -51,25 +93,37 @@ export const Header = ({
         />
       </Link>
   
-      <div className="header__nav" style={{color : textColorHeader}}>
-        <Link className="header__nav--link" to="/about">
-          About
-        </Link>
-        <Link className="header__nav--link" to="/projects">
-          Projects
-        </Link>
-        <Link className="header__nav--link" to="/careers">
-          Careers
-        </Link>
-        <Link className="header__nav--link" to="/philosophy">
-          Philosophy
-        </Link>
-        <Link className="header__nav--link" to="/news">
-          News
-        </Link>
-        <Link className="header__nav--link" to="/rewards">
-          Rewards
-        </Link>
+      <div className="header__nav" style={{ color : textColorHeader }}>
+        <div className="header__nav--link">
+          <Link to="/about" activeClassName="active">
+            About
+          </Link>
+        </div>
+        <div className="header__nav--link">
+          <Link to="/projects" activeClassName="active">
+            Projects
+          </Link>
+        </div>
+        <div className="header__nav--link">
+          <Link to="/careers" activeClassName="active">
+            Careers
+          </Link>
+        </div>
+        <div className="header__nav--link">
+          <Link to="/philosophy" activeClassName="active">
+            Philosophy
+          </Link>
+        </div>
+        <div className="header__nav--link">
+          <Link to="/news" activeClassName="active">
+            News
+          </Link>
+        </div>
+        <div className="header__nav--link">
+          <Link to="/rewards" activeClassName="active">
+            Rewards
+          </Link>
+        </div>
       </div>
 
       <div className="header__toggle">
