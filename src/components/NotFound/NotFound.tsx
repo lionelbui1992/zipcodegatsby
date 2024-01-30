49e4b0ad22afa9e310e1bfc930754abd9c6769b9
gianglx <gianglx@onextdigital.com>
2024-01-30 11:30:47 +0700

    MAASI2404ZIP-33 Error page + MAASI2404ZIP-34 Privacy

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -5,16 +5,28 @@ import { Link } from "gatsby";
 export const NotFound = (): JSX.Element => {
     return (
         <>
-            <main className="page-404 not-found text-center">
-                <div className="container">
-                    <h1>404.</h1>
-                    <h5>Page not Found.</h5>
-                    <p>
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis.
-                    </p>
-                    <Link className="btn" to="/">Back to Home</Link>
-                </div>
-            </main>
+            <div className="page-404 not-found text-center">
+                <section className="section-404">
+                    <div className="section-bkg">
+                        <img
+                            loading="lazy"
+                            srcSet="/img/page-privacy-policy-bkg.png"
+                        />
+                    </div>
+                    <div className="container">
+                        <h1>404.</h1>
+                    </div>
+                    <div className="main-content">
+                        <div className="container">
+                            <h5>Page not Found.</h5>
+                            <p>
+                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis.
+                            </p>
+                            <Link className="btn" to="/">Back to Home</Link>
+                        </div>
+                    </div>
+                </section>
+            </div>
         </>
     );
 }
