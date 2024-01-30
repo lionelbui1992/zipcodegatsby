b5328e0af3088df97cc35a5910fedc676b8907dd
HoangTN <hoangtn@arrowhitech.co>
2024-01-30 18:23:13 +0700

    update scroll homepage

--- src/templates/layout.tsx
+++ src/templates/layout.tsx
@@ -1,16 +1,50 @@
 import * as React from "react"
+import { useRef } from "react";
 import { Preload, ClipPath } from "../components/Preload"
 import { HeaderWrapper } from "../components/HeaderWrapper"
 import { FooterWrapper } from "../components/FooterWrapper"
+import { gsap } from 'gsap';
+import { ScrollSmoother } from "scroll-smoother-dev";
+import { useGSAP } from '@gsap/react';
 
-const Layout = ({ children }: { children: React.ReactNode }) => <div className="preload loading">
-    <Preload />
-    <div className="global-wrapper">
-        <HeaderWrapper />
-        <main>{children}</main>
-        <FooterWrapper />
-    </div>
-    <ClipPath />
-</div>
+gsap.registerPlugin(useGSAP, ScrollSmoother);
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+    const main = useRef();
+    const smoother = useRef();
+    useGSAP(
+        () => {
+            smoother.current = ScrollSmoother.create({
+                smooth: 2, // seconds it takes to "catch up" to native scroll position
+                effects: true, // look for data-speed and data-lag attributes on elements and animate accordingly
+            });
+            // ScrollTrigger.create({
+            //     trigger: '.item6',
+            //     pin: true,
+            //     start: 'center center',
+            //     end: '+=300',
+            //     markers: true,
+            // });
+        },
+        { scope: main }
+    );
+
+    return (
+        <div className="scrollWraper viewport" ref={main}>
+            <div className="preload loading">
+                <Preload />
+                <div className="global-wrapper">
+                    <HeaderWrapper />
+                    <main>{children}</main>
+                    <FooterWrapper />
+                </div>
+                <ClipPath />
+            </div>
+        </div>
+    )
+
+}
+
+
 
 export default Layout
