5d64786897b356a9fd313a3171eae25922849f42
ducmonmz <ducmonmz@gmail.com>
2024-01-30 09:36:13 +0700

    Add missing page

---
+++ src/pages/rewards.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+import type { HeadFC, PageProps } from "gatsby"
+
+const Rewards: React.FC<PageProps> = () => {
+  return (
+    ''
+  )
+}
+
+export default Rewards
+
+export const Head: HeadFC = () => <title>Rewards</title>
