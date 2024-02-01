c7f54a2ced44c4d638f155b39c5b5d6ed47ce7c2
gianglx <gianglx@onextdigital.com>
2024-02-01 17:06:52 +0700

    Project Page update code

--- src/components/ProjectsPage/BoxContent/BoxContent.tsx
+++ src/components/ProjectsPage/BoxContent/BoxContent.tsx
@@ -10,7 +10,7 @@ export const BoxContent = (): JSX.Element => {
                     <div className="column-box">
                         <h2 className="title">Design-Driven. <br />Human-Centered. <br />Category-Challenging. </h2>
                         <div className="content">
-                            <div className="description visible-desktop">Places and spaces have never been as vital as they are today. Because in an increasingly virtual world, something real is of even greater value, not just as an asset but for our growth and development. Because how and why we make things is as important as what we make.</div>
+                            <div className="description">Places and spaces have never been as vital as they are today. Because in an increasingly virtual world, something real is of even greater value, not just as an asset but for our growth and development. Because how and why we make things is as important as what we make.</div>
                             <Link className="btn btn-primary" to={`#`}>Our Design & Build Philosophy</Link>
                         </div>
                     </div>
