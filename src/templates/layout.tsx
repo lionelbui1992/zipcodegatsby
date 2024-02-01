ca0456129d2889459a96a061dd6cce809cc2c945
hains1088 <hains@onextdigital.com>
2024-02-01 09:20:43 +0700

    revert file layout

--- src/templates/layout.tsx
+++ src/templates/layout.tsx
@@ -12,16 +12,16 @@ gsap.registerPlugin(useGSAP, ScrollSmoother);
 const Layout = ({ children }: { children: React.ReactNode }) => {
     const main = useRef();
     const smoother = useRef();
-    // useGSAP(
-    //     () => {
-    //         smoother.current = ScrollSmoother.create({
-    //             smooth: 2, // seconds it takes to "catch up" to native scroll position
-    //             effects: true, // look for data-speed and data-lag attributes on elements and animate accordingly
-    //         });
+    useGSAP(
+        () => {
+            smoother.current = ScrollSmoother.create({
+                smooth: 2, // seconds it takes to "catch up" to native scroll position
+                effects: true, // look for data-speed and data-lag attributes on elements and animate accordingly
+            });
 
-    //     },
-    //     { scope: main }
-    // );
+        },
+        { scope: main }
+    );
 
     return (
         <div className="scrollWraper viewport" ref={main}>
