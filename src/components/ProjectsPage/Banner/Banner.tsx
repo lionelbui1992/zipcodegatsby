2fa4fd544a40f191f5cbf2107cf1025d291567e9
gianglx <gianglx@onextdigital.com>
2024-02-02 16:08:22 +0700

    Pre-load animation + Project

--- src/components/ProjectsPage/Banner/Banner.tsx
+++ src/components/ProjectsPage/Banner/Banner.tsx
@@ -4,6 +4,7 @@ import { Link } from "gatsby";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 
+
 export const Banner = (): JSX.Element => {
     useEffect(() => {
         const modalBtns: HTMLElement[] = Array.from(document.querySelectorAll(".projects-popup-item"));
@@ -27,6 +28,7 @@ export const Banner = (): JSX.Element => {
                 });
                 btn.classList.add('active-popup');
                 document.getElementById(modal)?.classList.add('active-popup');
+                document.querySelector('html')!.classList.add('active-overlay');
             }
         });
 
@@ -39,12 +41,14 @@ export const Banner = (): JSX.Element => {
                     element.classList.remove('active-popup');
                 });
                 modal?.classList.remove('active-popup');
+                document.querySelector('html')!.classList.remove('active-overlay');
             }
         });
     })
     return (
         <>
             <section className="projects-banner projects-section">
+                <div className="poup-overlay"></div>
                 <div className="section-bkg">
                     <img loading="lazy" srcSet="/img/page-privacy-policy-bkg.png" />
                 </div>
@@ -124,7 +128,7 @@ export const Banner = (): JSX.Element => {
                                 >
                                     <SwiperSlide>
                                         <div className="item-gallery">
-                                            <img loading="lazy" srcSet="/projects/ProjectKoala-image-1.jpg" />
+                                            <img loading="lazy" srcSet="/projects/ProjectKoala-image-1.png" />
                                         </div>
                                         <div className="popup-content visible-mobile">
                                             <h6>“Bangkok University's (BU) Best-in-class Student Residence”</h6>
@@ -132,7 +136,7 @@ export const Banner = (): JSX.Element => {
                                     </SwiperSlide>
                                     <SwiperSlide>
                                         <div className="item-gallery">
-                                            <img loading="lazy" srcSet="/projects/ProjectKoala-image-3.jpg" />
+                                            <img loading="lazy" srcSet="/projects/ProjectKoala-image-2.png" />
                                         </div>
                                         <div className="popup-content visible-mobile">
                                             <p>With Bangkok University's support and pre-owned land, Project Koala will offer a higher quality product that is truly integrated into the modern student lifestyle, uplifting the standard of living and learning for the student community.</p>
@@ -140,7 +144,7 @@ export const Banner = (): JSX.Element => {
                                     </SwiperSlide>
                                     <SwiperSlide>
                                         <div className="item-gallery">
-                                            <img loading="lazy" srcSet="/projects/ProjectKoala-image-2.jpg" />
+                                            <img loading="lazy" srcSet="/projects/ProjectKoala-image-3.png" />
                                         </div>
                                         <div className="popup-content visible-mobile">
                                             <p>Without a university-backed student residence and over 37,000 students, there is a clear unaddressed gap in the market and offering.</p>
@@ -201,7 +205,7 @@ export const Banner = (): JSX.Element => {
                                 >
                                     <SwiperSlide>
                                         <div className="item-gallery">
-                                            <img loading="lazy" srcSet="/projects/ProjectKanga-image-1.jpg" />
+                                            <img loading="lazy" srcSet="/projects/ProjectKanga-image-1.png" />
                                         </div>
                                         <div className="popup-content visible-mobile">
                                 <p><span className="h6">“Bangkok University's (BU) Best-in-class Student Residence”</span> in the heart of Bangkok, Kanga is Hybrid Space for Productivity, Creativity and Lifelong Learning. </p>
@@ -209,7 +213,7 @@ export const Banner = (): JSX.Element => {
                                     </SwiperSlide>
                                     <SwiperSlide>
                                         <div className="item-gallery">
-                                            <img loading="lazy" srcSet="/projects/ProjectKanga-image-2.jpg" />
+                                            <img loading="lazy" srcSet="/projects/ProjectKanga-image-2.png" />
                                         </div>
                                         <div className="popup-content visible-mobile">
                                             <p>A mixed-use development offering new generation approach to of office spaces, learning zone, art spaces, retail, food & beverage along with an   active zone for sports and pet zone. </p>
@@ -217,7 +221,7 @@ export const Banner = (): JSX.Element => {
                                     </SwiperSlide>
                                     <SwiperSlide>
                                         <div className="item-gallery">
-                                            <img loading="lazy" srcSet="/projects/ProjectKanga-image-3.jpg" />
+                                            <img loading="lazy" srcSet="/projects/ProjectKanga-image-3.png" />
                                         </div>
                                         <div className="popup-content visible-mobile">
                                             <p>Inspired to serve the needs of the city, Kanga offers a wide range of unique activities for you and your important ones to have fun, recharge & relax, learn, and discover the undiscovered. </p>
@@ -278,7 +282,7 @@ export const Banner = (): JSX.Element => {
                                 >
                                     <SwiperSlide>
                                         <div className="item-gallery">
-                                            <img loading="lazy" srcSet="/projects/ProjectHeyday-image-1.jpg" />
+                                            <img loading="lazy" srcSet="/projects/ProjectHeyday-image-1.png" />
                                         </div>
                                         <div className="popup-content visible-mobile">
                                             <p>We provided amenities and shops that will <strong>recharge student’s batteries after a long day on campus.</strong></p>
@@ -286,7 +290,7 @@ export const Banner = (): JSX.Element => {
                                     </SwiperSlide>
                                     <SwiperSlide>
                                         <div className="item-gallery">
-                                            <img loading="lazy" srcSet="/projects/ProjectHeyday-image-2.jpg" />
+                                            <img loading="lazy" srcSet="/projects/ProjectHeyday-image-2.png" />
                                         </div>
                                         <div className="popup-content visible-mobile">
                                             <p>With easy access within walking distance from the campus, students will enjoy our shops and restaurants, shared dining space, co-working space, and outdoor area with events and workshops that support their lifestyle activities.</p>
@@ -294,7 +298,7 @@ export const Banner = (): JSX.Element => {
                                     </SwiperSlide>
                                     <SwiperSlide>
                                         <div className="item-gallery">
-                                            <img loading="lazy" srcSet="/projects/ProjectHeyday-image-3.jpg" />
+                                            <img loading="lazy" srcSet="/projects/ProjectHeyday-image-3.png" />
                                         </div>
                                         <div className="popup-content visible-mobile">
                                             <p>More text needed</p>
@@ -355,7 +359,7 @@ export const Banner = (): JSX.Element => {
                                 >
                                     <SwiperSlide>
                                         <div className="item-gallery">
-                                            <img loading="lazy" srcSet="/projects/ProjectHeyday-image-1.jpg" />
+                                            <img loading="lazy" srcSet="/projects/ProjectHeyday-image-1.png" />
                                         </div>
                                         <div className="popup-content visible-mobile">
                                             <p>We provided amenities and shops that will <strong>recharge student’s batteries after a long day on campus.</strong></p>
@@ -363,7 +367,7 @@ export const Banner = (): JSX.Element => {
                                     </SwiperSlide>
                                     <SwiperSlide>
                                         <div className="item-gallery">
-                                            <img loading="lazy" srcSet="/projects/ProjectHeyday-image-2.jpg" />
+                                            <img loading="lazy" srcSet="/projects/ProjectHeyday-image-2.png" />
                                         </div>
                                         <div className="popup-content visible-mobile">
                                             <p>With easy access within walking distance from the campus, students will enjoy our shops and restaurants, shared dining space, co-working space, and outdoor area with events and workshops that support their lifestyle activities.</p>
@@ -371,7 +375,7 @@ export const Banner = (): JSX.Element => {
                                     </SwiperSlide>
                                     <SwiperSlide>
                                         <div className="item-gallery">
-                                            <img loading="lazy" srcSet="/projects/ProjectHeyday-image-3.jpg" />
+                                            <img loading="lazy" srcSet="/projects/ProjectHeyday-image-3.png" />
                                         </div>
                                         <div className="popup-content visible-mobile">
                                             <p>More text needed</p>
