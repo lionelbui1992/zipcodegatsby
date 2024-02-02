55e7a2f314983a824f2589d15eedec22c0ab72aa
ducmonmz <ducmonmz@gmail.com>
2024-02-02 10:33:15 +0700

    format & update config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,10 +1,14 @@
 import type { GatsbyConfig } from "gatsby";
+// support for .env, .env.development, and .env.production
+require("dotenv").config();
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `zipcode`,
+    author: `Onextdigital`,
+    description: "Zipcode application",
     siteUrl: process.env.SITE_URL
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
@@ -16,7 +20,11 @@ const config: GatsbyConfig = {
     options: {
       "url": process.env.WPGRAPHQL_URL
     }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass"]
-};
+  },
+  "gatsby-plugin-image",
+  "gatsby-plugin-sharp",
+  "gatsby-transformer-sharp",
+  "gatsby-plugin-sass"
+]};
 
 export default config;
