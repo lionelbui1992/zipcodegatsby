3151f679c124d74b8fc2a6c9957b2c441870508e
gianglx <gianglx@onextdigital.com>
2024-02-07 10:54:54 +0700

    Career page update code

--- src/components/Careers/Life/Life.tsx
+++ src/components/Careers/Life/Life.tsx
@@ -1,63 +1,108 @@
 import React from "react";
 import "./style.sass";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
 
 interface Props {
     lifeTitle: string;
     lifeBackground: string;
+    lifeImagePlaceholder1: string;
+    lifeImagePlaceholder2: string;
+    lifeIconRow: string;
+    lifeIconCol: string;
+    lifeGallery: any[];
 }
 
 export const Life = ({
     lifeTitle,
     lifeBackground,
+    lifeIconRow,
+    lifeIconCol,
+    lifeImagePlaceholder1,
+    lifeImagePlaceholder2,
+    lifeGallery,
 }: Props): JSX.Element => {
+    const galleryContent: HTMLElement | null = document.querySelector('.section-gallery-content');
+
+    // document.onclick = (e: MouseEvent): void => {
+    //     if (e.target && (e.target as HTMLElement).id !== 'life-gallery-popup' && galleryContent?.classList.contains('gallery-popup-active')) {
+    //         galleryContent?.classList.remove('gallery-popup-active');
+    //     }
+    // };
+
+    // const openPopup = () => {
+    //     galleryContent?.classList.add('gallery-popup-active');
+    // }
+
     return (
-        <div className="careers-life careers-section" style={{backgroundImage: "url("+lifeBackground+")"}}>
-            <div className="container">
-                <div className="section-title">
-                    <h2 className="h4" dangerouslySetInnerHTML={{__html: lifeTitle}} />
+        <>
+            { (lifeTitle || lifeGallery) && 
+                <div className="careers-life careers-section" style={{backgroundImage: "url("+lifeBackground+")"}}>
+                    <div className="container">
+                        <div className="section-title">
+                            <h2 className="h4" dangerouslySetInnerHTML={{__html: lifeTitle}} />
+                        </div>
+                        { lifeGallery && 
+                            <div className="section-content section-gallery-content">
+                                <div className="life-gallery-wrapper">
+                                    { lifeIconRow && 
+                                        <div className="gallery-icon gallery-icon-row">
+                                            <img loading="lazy" srcSet={lifeIconRow} />
+                                        </div>
+                                    }
+                                    { lifeIconCol && 
+                                        <div className="gallery-icon gallery-icon-col">
+                                            <img loading="lazy" srcSet={lifeIconCol} />
+                                        </div>
+                                    }
+                                    <div className="life-gallery">
+                                        { lifeGallery.map((list, index) => (
+                                            (list.imgUrl) && 
+                                                <div className="item" data-index={index} key={index} onClick={() => openPopup()}>
+                                                    <div className="item-image">
+                                                        {((index + 1) % 6 == 2 || (index + 1) % 6 == 4) ? (
+                                                            <img className="img-placeholder" loading="lazy" srcSet={lifeImagePlaceholder2} alt="" />
+                                                        ) : (
+                                                            <img className="img-placeholder" loading="lazy" srcSet={lifeImagePlaceholder1} alt="" />
+                                                        )}
+                                                        <img className="img-default" loading="lazy" srcSet={list.imgUrl} alt="" />
+                                                    </div>
+                                                </div>
+                                        ))}
+                                    </div>
+                                </div>
+                                <div className="life-gallery-popup" id="life-gallery-popup">
+                                    <Swiper
+                                        modules={[Navigation]}
+                                        spaceBetween={20}
+                                        slidesPerView={1}
+                                        navigation
+                                        pagination={{
+                                            el: "#containerForBullets",
+                                            type: "bullets",
+                                            bulletClass: "swiper-custom-bullet",
+                                            bulletActiveClass: "swiper-custom-bullet-active",
+                                            clickable: true,
+                                        }}
+                                        >
+                                        { lifeGallery.map((list, index) => (
+                                            (list.imgUrl) && 
+                                            <SwiperSlide key={index} data-index={index}>
+                                                <div className="item">
+                                                    <div className="item-image">
+                                                        <img loading="lazy" srcSet={list.imgUrl} alt="" />
+                                                    </div>
+                                                </div>
+                                            </SwiperSlide>
+                                        ))}
+                                    </Swiper>
+                                </div>
+                            </div>
+                        }
+                    </div>
                 </div>
-                <div className="section-content">
-                    <div className="gallery-icon gallery-icon-row">
-                        <img loading="lazy" srcSet="https://wordpress-897316-4088707.cloudwaysapps.com/headless/wp-content/uploads/2024/02/gallery-box-icon-row.svg" />
-                    </div>
-                    <div className="gallery-icon gallery-icon-col">
-                        <img loading="lazy" srcSet="https://wordpress-897316-4088707.cloudwaysapps.com/headless/wp-content/uploads/2024/02/gallery-box-icon-col.svg" />
-                    </div>
-                    <div className="life-gallery">
-                        <div className="item">
-                            <div className="item-image">
-                                <img loading="lazy" srcSet="https://wordpress-897316-4088707.cloudwaysapps.com/headless/wp-content/uploads/2024/02/gallery-image-1.png" />
-                            </div>
-                        </div>
-                        <div className="item">
-                            <div className="item-image">
-                                <img loading="lazy" srcSet="https://wordpress-897316-4088707.cloudwaysapps.com/headless/wp-content/uploads/2024/02/gallery-image-2.png" />
-                            </div>
-                        </div>
-                        <div className="item">
-                            <div className="item-image">
-                                <img loading="lazy" srcSet="https://wordpress-897316-4088707.cloudwaysapps.com/headless/wp-content/uploads/2024/02/gallery-image-3.png" />
-                            </div>
-                        </div>
-                        <div className="item">
-                            <div className="item-image">
-                                <img loading="lazy" srcSet="https://wordpress-897316-4088707.cloudwaysapps.com/headless/wp-content/uploads/2024/02/gallery-image-4.png" />
-                            </div>
-                        </div>
-                        <div className="item">
-                            <div className="item-image">
-                                <img loading="lazy" srcSet="https://wordpress-897316-4088707.cloudwaysapps.com/headless/wp-content/uploads/2024/02/gallery-image-5.png" />
-                            </div>
-                        </div>
-                        <div className="item">
-                            <div className="item-image">
-                                <img loading="lazy" srcSet="https://wordpress-897316-4088707.cloudwaysapps.com/headless/wp-content/uploads/2024/02/gallery-image-6.png" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+            }
+        </>
     );
 }
 export default Life;
--- src/components/Careers/OurValues/OurValues.tsx
+++ src/components/Careers/OurValues/OurValues.tsx
@@ -3,12 +3,16 @@ import "./styles.sass";
 
 interface Props {
     OurValuesTitle: string;
+    OurValuesImagePlaceholder1: string;
+    OurValuesImagePlaceholder2: string;
     OurValuesContents: any[];
 }
 
 export const OurValues = ({
     OurValuesTitle,
     OurValuesContents,
+    OurValuesImagePlaceholder1,
+    OurValuesImagePlaceholder2,
 }: Props): JSX.Element => {
     const numRows = OurValuesContents.length
     const numCol = numRows/2
@@ -33,9 +37,13 @@ export const OurValues = ({
                                                                 { list.imgUrl && 
                                                                     <div className="image-first">
                                                                         <div className="image-inner">
+                                                                            <img className="img-placeholder" 
+                                                                                loading="lazy" 
+                                                                                srcSet={OurValuesImagePlaceholder1} alt=""
+                                                                            />
                                                                             <img
                                                                                 loading="lazy"
-                                                                                srcSet={`${list.imgUrl}`} className="img"
+                                                                                srcSet={`${list.imgUrl}`} className="img img-default"
                                                                                 alt={list.title}
                                                                             />
                                                                         </div>
@@ -44,9 +52,13 @@ export const OurValues = ({
                                                                 { list.imgSecondUrl && 
                                                                     <div className="image-second">
                                                                         <div className="image-inner">
+                                                                            <img className="img-placeholder" 
+                                                                                loading="lazy" 
+                                                                                srcSet={OurValuesImagePlaceholder2} alt=""
+                                                                            />
                                                                             <img
                                                                                 loading="lazy"
-                                                                                srcSet={`${list.imgSecondUrl}`} className="img"
+                                                                                srcSet={`${list.imgSecondUrl}`} className="img img-default"
                                                                                 alt={list.title}
                                                                             />
                                                                         </div>
