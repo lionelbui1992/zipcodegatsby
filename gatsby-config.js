14f0104632f218196895bf2a4411b4ddb6a03fc7
ducmonmz <ducmonmz@gmail.com>
2024-02-15 17:02:03 +0700

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,14 @@ module.exports = {
         "url": process.env.WPGRAPHQL_URL
       }
     },
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
