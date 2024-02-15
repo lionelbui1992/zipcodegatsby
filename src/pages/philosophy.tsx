644c8988688d3cb4c6323595f969382142586687
hains1088 <hains@onextdigital.com>
2024-02-15 08:34:37 +0700

    Add overlap animation

--- src/pages/philosophy.tsx
+++ src/pages/philosophy.tsx
@@ -1,10 +1,15 @@
 import * as React from "react"
+import { useRef, useEffect } from "react";
 import { Banner } from "../components/Philosophy/Banner";
 import { ImageWithText } from "../components/Philosophy/ImageWithText";
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEOHead from "../components/head"
 import { IPageProps } from "../shared/model/IPageProps";
+import { gsap } from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Philosophy: React.FC<IPageProps> = (props: IPageProps) => {
   // const { page } = props.data
@@ -53,11 +58,65 @@ const Philosophy: React.FC<IPageProps> = (props: IPageProps) => {
       backgroundUrl: bgBlue,
       isDarkBackground: true,
     },
-  ]
+  ];
+  const lastItem = contents[contents.length - 1];
+  const container = useRef(null);
+  useEffect(() => {
 
+    if (!container) return;
+
+    const absoluteSections = document.querySelectorAll('.scroll-section .relative-section');
+    const totalHeight = [...absoluteSections].reduce((acc, section) => {
+      section.classList.replace('relative-section', 'absolute-section'); // Replace class
+      return acc + section.offsetHeight;
+    }, 0);;
+
+    document.querySelector('.pinning-2').style.height = totalHeight + 'px';
+    // const test = totalHeight + 'px 450px';
+
+    ScrollTrigger.create({
+      trigger: ".pinning-1",
+      start: "top top",
+      end: "bottom top",
+      pin: true,
+      pinSpacing: false,
+      markers: false,
+      scrub: false
+    });
+
+    let i = 0;
+    let j = 0;
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".pinning-2",
+        start: "top 300px",
+        end: totalHeight,
+        pin: true,
+        pinSpacing: false,
+        scrub: 0.001,
+        markers: true,
+      },
+      ease: "none",
+      smoothChildTiming: true
+    });
+
+
+    tl.to(".phi-1", { yPercent: -100 })
+    tl.to(".phi-2", { yPercent: -200 })
+    tl.to(".phi-3", { yPercent: -300 })
+    tl.to(".phi-4", { yPercent: -400 })
+    tl.to(".phi-5", { yPercent: -500 })
+
+
+    return () => {
+      // Kill all ScrollTriggers on unmount
+      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+    };
+  }, []);
   
   return (
     <Layout>
+      <div className="scroll-section pinning-1" data-speed="0.2">
       <Banner 
         label={label} 
         bannerTitle={bannerTitle} 
@@ -65,7 +124,11 @@ const Philosophy: React.FC<IPageProps> = (props: IPageProps) => {
         bannerImageUrl={bannerImageUrl} 
         bannerBackground={bannerBackground} 
       />
-      {contents.map((list, index) => (
+      </div>
+      <div className="scroll-section pinning-2 company" data-speed="0.3">
+      {contents.map((list, index, contents) => (
+        
+        <div className={`phi-${index+1} ${index+1 !== contents.length  ? "relative-section" : "absolute-section"} `} style={{zIndex: 100 - index}}>
         <ImageWithText 
           index={index + 1}
           title={list.title} 
@@ -74,7 +137,20 @@ const Philosophy: React.FC<IPageProps> = (props: IPageProps) => {
           backgroundUrl={list.backgroundUrl} 
           isDarkBackground={list.isDarkBackground}
         />
+        </div>
+        
       ))}
+      </div>
+      <div> 
+        <ImageWithText 
+          index={contents.length}
+          title={lastItem.title} 
+          des={lastItem.des} 
+          imgUrl={lastItem.imgUrl} 
+          backgroundUrl={lastItem.backgroundUrl} 
+          isDarkBackground={lastItem.isDarkBackground}
+        />
+      </div>
     </Layout>
   );
 }
