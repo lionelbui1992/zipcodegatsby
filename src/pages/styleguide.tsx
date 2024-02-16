6b0c14f25508cc51f787c64eb015cc6e88b58bef
ducmonmz <ducmonmz@gmail.com>
2024-02-16 22:15:38 +0700

    Fix bug build production

--- src/pages/styleguide.tsx
+++ src/pages/styleguide.tsx
@@ -1,12 +1,8 @@
 import * as React from "react"
 import { StyleGuide } from "../components/StyleGuide";
-import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import SEOHead from "../components/head"
-import { IPageProps } from "../shared/model/IPageProps";
 
-const StyleGuidePage: React.FC<IPageProps> = (props: IPageProps) => {
-    // const { page } = props.data
+const StyleGuidePage: React.FC = () => {
     return (
         <Layout>
             <StyleGuide />
@@ -15,23 +11,3 @@ const StyleGuidePage: React.FC<IPageProps> = (props: IPageProps) => {
 }
 
 export default StyleGuidePage
-
-export const Head = (props: IPageProps) => {
-  const { page } = props.data
-  return <SEOHead {...page} />
-}
-export const query = graphql`
-  query PageContent($id: String!) {
-    page(id: { eq: $id }) {
-      id
-      title
-      slug
-      description
-      image {
-        id
-        url
-      }
-      html
-    }
-  }
-`
