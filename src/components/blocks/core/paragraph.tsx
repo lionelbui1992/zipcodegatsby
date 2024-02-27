806c4a7fc40ca5e1bbf3ec3709189294dfbcc64a
ducmonmz <ducmonmz@gmail.com>
2024-02-27 15:14:07 +0700

    add columns custom classes

--- src/components/blocks/core/paragraph.tsx
+++ src/components/blocks/core/paragraph.tsx
@@ -2,14 +2,17 @@ import React from "react";
 
 export interface CoreParagraphProps {
   attributes: {
+    className?: string
     content: string
   }
 }
 
 export default function CoreParagraph(props: CoreParagraphProps): JSX.Element {
-  const { attributes: { content } } = props;
+  const { attributes: { className, content } } = props;
+
+  const classes = className ? `wp-block-paragraph ${className}` : 'wp-block-paragraph';
   
   return (
-    <p dangerouslySetInnerHTML={{ __html: content }} />
+    <p className={classes} dangerouslySetInnerHTML={{ __html: content }} />
   );
 };
