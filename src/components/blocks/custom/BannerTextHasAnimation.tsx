0c452643baa73746e4295e72c6d5189edad4027b
gianglx <gianglx@onextdigital.com>
2024-03-08 11:23:54 +0700

    MAASI2404ZIP-150 Can we make the headline & image a little bit bigger on desktop

--- src/components/blocks/custom/BannerTextHasAnimation.tsx
+++ src/components/blocks/custom/BannerTextHasAnimation.tsx
@@ -4,14 +4,19 @@ import { IBannerTextHasAnimationProps } from "../types";
 
 export const BannerTextHasAnimation = ({blockName, attributes} : {blockName: string, attributes: IBannerTextHasAnimationProps}): JSX.Element => {
     const { label, texts } = attributes;
+    const textsSize = texts.length / 2;
     useEffect(() => {
         const ttContent: HTMLElement | null = document.querySelector(".content-tooltip");
         const ttImages: NodeListOf<HTMLElement> = document.querySelectorAll('.tt-image');
+        console.log(ttContent?.length);
 
         function ttImagesTooltip() {
+            const ttContentHeight = ttContent?.offsetHeight;
             ttImages.forEach((ttImage: HTMLElement) => {
                 const ttImageTop: number = ttImage.offsetTop;
+                const ttImageBottom: number = ttContentHeight - ttImageTop;
                 ttImage?.style.setProperty('--offsetTop', `${ttImageTop}px`);
+                ttImage?.style.setProperty('--offsetBottom', `${ttImageBottom}px`);
                 if (window.innerWidth > 768) {
                     ttImage.addEventListener('mouseover', () => {
                         ttImage.classList.add('active');
@@ -24,6 +29,9 @@ export const BannerTextHasAnimation = ({blockName, attributes} : {blockName: str
                     }) 
                 }         
                 ttImage.addEventListener('mouseleave', () => {
+                    document.querySelectorAll('.tt-image').forEach((image: HTMLElement) => {
+                        image.classList.remove('active');
+                    });
                     ttImage.classList.remove('active');
                     ttContent?.classList.remove('tooltip-active');
                 })
@@ -45,7 +53,7 @@ export const BannerTextHasAnimation = ({blockName, attributes} : {blockName: str
                             return (
                                 text.text && (
                                     text.image ? 
-                                        <><span className="tt-image" key={index}>{text.text}<img loading="lazy" srcSet={text.image.src} alt="" /></span> </>
+                                        <><span className={`tt-image ${(index) > textsSize ? 'position-top' : ''}`} key={index}>{text.text}<img loading="lazy" srcSet={text.image.src} alt="" /></span> </>
                                     :
                                         <>{text.text} </>
                                 )
