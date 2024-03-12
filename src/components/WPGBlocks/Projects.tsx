0641e1cc293f117d59cdaccdc319b0fe5bf61835
hoangtn <hoangtn@onextdigital.com>
2024-03-12 09:52:36 +0700

    update animation

--- src/components/WPGBlocks/Projects.tsx
+++ src/components/WPGBlocks/Projects.tsx
@@ -3,14 +3,27 @@ import { GetTheBlock } from '../blocks'
 import { IWPGBlocksProps, IWPGBlockProps } from './types'
 
 const ProjectsBlocks: React.FunctionComponent<IWPGBlocksProps> = ({ blocks, mapToBlock }) => {
+
+    const firstAnimatoinBlocks = blocks.filter((block, index) => {
+        return block.name === "acf/projects-banner" || block.name === "acf/marquee"
+    });
+
     return (
-        <>
+        <div className='page-content'>
+            <div className="overlay-animation">
+                {firstAnimatoinBlocks.filter(block => {
+                    return !!block.name
+                }).map((block, index) =>
+                    <ProjectsBlock key={index} order={`${index}`} block={block} mapToBlock={mapToBlock} />
+                )}
+            </div>
+
             {blocks.filter(block => {
-                return !!block.name
+                return !!block.name && (block.name !== "acf/projects-banner" && block.name !== "acf/marquee")
             }).map((block, index) =>
-                <PhilosophyBlock key={index} order={`${index}`} block={block} mapToBlock={mapToBlock} />
+                <ProjectsBlock key={index} order={`${index}`} block={block} mapToBlock={mapToBlock} />
             )}
-        </>
+        </div>
     )
 }
 
@@ -30,19 +43,19 @@ export const ProjectsBlock: React.FunctionComponent<IWPGBlockProps> = ({ block, 
 
     if (!TheBlock) return null
     switch (name) {
-        case 'acf/banner-with-image-right':
-            return (
-                <div className="phi-banner overlay-animation">
-                    <TheBlock blockName={name} attributes={attributes.data} />
-                </div>
-            )
-        case 'acf/number-text-image-repeater':
-            return (
-                <TheBlock blockName={name} attributes={attributes.data} />
-            )
+        // case 'acf/banner-with-image-right':
+        //     return (
+        //         <div className="phi-banner overlay-animation">
+        //             <TheBlock blockName={name} attributes={attributes.data} />
+        //         </div>
+        //     )
+        // case 'acf/number-text-image-repeater':
+        //     return (
+        //         <TheBlock blockName={name} attributes={attributes.data} />
+        //     )
         default:
             return (
-                <TheBlock blockName={name} attributes={attributes} />
+                <TheBlock blockName={name} attributes={attributes.data} />
             )
     }
 }
