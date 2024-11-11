04ad06d81253838a547f9d4900521fe8f63084ee
donglv1 <lovandong342@gmail.com>
2024-11-11 17:37:06 +0700

    update code

--- src/components/blocks/custom/BannerTextCenter.tsx
+++ src/components/blocks/custom/BannerTextCenter.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { IBannerTextCenterProps } from "../types";
+import { useLang } from "../../../context/LangContext";
 
 const BannerTextCenter = ({ attributes }: { attributes: IBannerTextCenterProps }): JSX.Element => {
     const { background, heading,sub_heading } = attributes;
+    const {language} = useLang();
     const appHeight = () => {
         const doc = document.documentElement
         doc.style.setProperty('--screen-height', `${window.innerHeight}px`)
@@ -13,8 +15,11 @@ const BannerTextCenter = ({ attributes }: { attributes: IBannerTextCenterProps }
     return (
         <div className="section section-banner">
             <div className="inner-section" style={{ backgroundImage: `url(${background.src})` }}>
-                <h1 className="heading">{heading}</h1>
-                <h3 className="sub-heading">{sub_heading}</h3>
+                <h1 className="heading">
+                    {heading}
+                    <br />
+                    <span className="sub-heading">{sub_heading || language === 'th' ? 'เราสร้างอย่างแตกต่าง':''}</span>
+                </h1>
             </div>
         </div>
     );
